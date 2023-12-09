07e3a0068f442347fc4d052494ae88e2cc73e3e0
ARProxy <hocci0222@kakao.com>
2023-12-09 16:43:24 +0900

    Initial commit

---
+++ src/main/java/com/flow/controller/FlowController.java
@@ -0,0 +1,18 @@
+package com.flow.controller;
+
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@NoArgsConstructor
+@RequestMapping("/")
+public class FlowController {
+
+    @GetMapping(value = "flow")
+    public String flowTest() {
+        return "flow";
+    }
+}
